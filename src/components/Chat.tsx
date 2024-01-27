4cfa54c93faffa44302cb2652c35507fb6393c41
Kot Shrodingera <kot.shrodingera@gmail.com>
2024-01-27 20:55:41 +0100

    Chat: work with ws server

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { cn } from '../lib/utils'
 import ChatMessage, { type ChatMessageProps } from './ChatMessage'
 import Button from './ui/Button'
@@ -8,29 +8,63 @@ type ChatProps = {
   className: string
 }
 
+type Status = 'openned' | 'closed'
+
 function Chat({ className }: ChatProps) {
-  const [messages] = useState<ChatMessageProps[]>([
-    {
-      sender: 'You',
-      text: 'Yours message',
-      datetime: new Date('January 27, 2024, 10:00 AM'),
-    },
-    {
-      sender: 'Server',
-      text: 'Servers message',
-      datetime: new Date('January 27, 2024, 10:05 AM'),
-    },
-    {
-      sender: 'You',
-      text: 'Yours very very very very very very very very very long message',
-      datetime: new Date('January 27, 2024, 10:10 AM'),
-    },
-    {
-      sender: 'Server',
-      text: 'Servers very very very very very very very very very long message',
-      datetime: new Date('January 27, 2024, 10:15 AM'),
-    },
-  ])
+  const [messages, setMessages] = useState<ChatMessageProps[]>([])
+  const [status, setStatus] = useState<Status>('closed')
+
+  const isOpenned = status === 'openned'
+
+  const addMessage = (message: string, sender: string) => {
+    setMessages((prev) => [
+      ...prev,
+      {
+        sender,
+        text: message,
+        datetime: new Date(),
+      },
+    ])
+  }
+
+  const connection = useRef<WebSocket | null>(null)
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:5000')
+    connection.current = socket
+
+    socket.addEventListener('open', () => {
+      console.log('Socket openned')
+      setStatus('openned')
+    })
+
+    socket.addEventListener('message', (event) => {
+      addMessage(event.data as string, 'Server')
+    })
+
+    socket.addEventListener('close', () => {
+      console.log('Socket closed')
+      setStatus('closed')
+    })
+
+    return () => {
+      socket.close()
+      connection.current = null
+    }
+  }, [])
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const input = event.currentTarget.elements.namedItem(
+      'message',
+    ) as HTMLTextAreaElement
+    const message = input.value
+    if (connection.current !== null) {
+      addMessage(message, 'You')
+      connection.current.send(message)
+      input.value = ''
+    }
+  }
 
   return (
     <div className={cn([className, 'flex h-full flex-col'])}>
@@ -39,10 +73,24 @@ function Chat({ className }: ChatProps) {
           <ChatMessage key={index} {...message} />
         ))}
       </div>
-      <div className="flex gap-2 border-t p-4">
-        <TextArea className="flex-1" placeholder="Type your message here" />
-        <Button>Send</Button>
+      <div
+        className={cn([
+          'flex justify-center p-2 text-sm font-bold',
+          isOpenned ? 'text-green-500' : 'text-red-500',
+        ])}
+      >
+        {isOpenned ? 'Connected' : 'Not Connected'}
       </div>
+      <form className="flex gap-2 border-t p-4" onSubmit={handleSubmit}>
+        <TextArea
+          className="flex-1"
+          placeholder="Type your message here"
+          name="message"
+        />
+        <Button type="submit" disabled={!isOpenned}>
+          Send
+        </Button>
+      </form>
     </div>
   )
 }
