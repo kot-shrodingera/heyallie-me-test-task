9485ea7d97fcb45ab6ed2939768f10d0b870cd0e
Kot Shrodingera <kot.shrodingera@gmail.com>
2024-01-28 07:44:36 +0100

    Set up vitest env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.ts',
+  },
 })
